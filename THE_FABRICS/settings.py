afc21bd3ec87d21cbd88d81a194f1d9288479d31
naman153 <153namangupta@gmail.com>
2021-05-18 10:54:13 +0530

    commit

--- THE_FABRICS/settings.py
+++ THE_FABRICS/settings.py
@@ -31,7 +31,6 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','the-fabric-store.herokuapp.com']
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -125,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
